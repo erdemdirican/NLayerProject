58b858bb405ea7558633a32823386d5072037e40
Erdem Dirican <erdemdirican@hotmail.com>
2021-06-24 22:03:34 +0300

    Add project files.

---
+++ NLayerProject.API/Controllers/PersonsController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayerProject.API.DTOs;
+using NLayerProject.Core.Models;
+using NLayerProject.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NLayerProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private readonly IService<Person> _personService;
+        private readonly IMapper _mapper;
+        public PersonsController(IService<Person> service, IMapper mapper)
+        {
+            _personService = service;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var persons = await _personService.GetAllAsync();
+
+            return Ok(_mapper.Map<IEnumerable<PersonDto>>(persons));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(PersonDto person)
+        {
+            var newPerson = await _personService.AddAsync(_mapper.Map<Person>(person));
+
+            return Created(string.Empty, _mapper.Map<PersonDto>(newPerson));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remove(int id)
+        {
+            var person = _personService.GetByIdAsync(id).Result;
+            _personService.Remove(person);
+
+            return NoContent();
+        }
+    }
+}
---
+++ NLayerProject.Data/Migrations/20210618114954_AddAge.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NLayerProject.Data.Migrations
+{
+    public partial class AddAge : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Persons",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Persons");
+        }
+    }
+}
